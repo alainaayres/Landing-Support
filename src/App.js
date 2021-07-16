076bb105ce18c8e96ed6d7608b9908b4a02fa8bc
Alaina Ayres <alainaayres@gmail.com>
2021-07-15 17:14:16 -0700

    test github integration

--- src/App.js
+++ src/App.js
@@ -6,21 +6,9 @@ import Demo from './components/body/demo.js';
 import Footer from './components/footer/footer.js';
 import Contacts from './components/body/contacts.js';
 import { useMediaQuery } from 'react-responsive';
-import BigScreen from './components/BigScreen.js';
-import Desktop from './components/Desktop.js';
-import Laptop from './components/Laptop.js';
-import Tablet from './components/Tablet.js';
-import Mobile from './components/Mobile.js';
 
 const App = () => {
-  const isDesktopOrLaptop = useMediaQuery({
-    query: '(min-device-width: 1224px)'
-  })
-  const isBigScreen = useMediaQuery({ query: '(min-device-width: 1824px)' })
-  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
-  const isTabletOrMobileDevice = useMediaQuery({
-    query: '(max-device-width: 1224px)'
-  })
+
   const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
   const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' })
 
